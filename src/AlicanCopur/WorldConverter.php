1b0f61b325dedb7655f2ad65dc9f3e179750f18e
AlicanCopur <37244649+AlicanCopur@users.noreply.github.com>
2021-06-05 21:33:32 +0300

    update after 2 years

--- src/AlicanCopur/WorldConverter.php
+++ src/AlicanCopur/WorldConverter.php
@@ -12,7 +12,7 @@
 *
 * @version 1.0
 * @author AlicanCopur
-* @copyright HashCube Network © | 2015 - 2019
+* @copyright HashCube Network © | 2015 - 2021
 * @license Açık yazılım lisansı altındadır. Tüm hakları saklıdır. 
 */                                   
 
@@ -25,22 +25,26 @@ use pocketmine\{
         level\Level
 };
 
-class WorldUpdater extends PluginBase {
+class WorldConverter extends PluginBase {
 	
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args): bool{
-		$this->startConversion($sender);
+		$this->startConversion();
 		return true;
 	}
-	private function startConversion(CommandSender $sender): void{
+	private function startConversion(): void{
+		//TODO: better way instead of 'system()'
 		system("cd ".$this->getServer()->getDataPath());
 		$levels = scandir($this->getServer()->getDataPath()."worlds");
 		foreach($levels as $level){
 		    if($level == "." || $level == "..") continue;
 			$this->convert($level);
 		}
-		$sender->sendMessage("Conversion successfully finished!");
+		$this->getLogger()->info("=================================");
+		$this->getLogger()->info("Conversion successfully finished!");
+		$this->getLogger()->info("=================================");
 	}
 	public function convert($level): void{
+		$this->getLogger()->info("Level " . $level . " is converting...");
 		$world = $this->getServer()->getLevelByName($level);
                 if($world instanceof Level) $world->unload();
 		$this->anvilToRegion($level);
@@ -54,11 +58,12 @@ class WorldUpdater extends PluginBase {
     		if($format == "mca" || $format == "mcr")
     			unlink($dir.$file);
 		}
-		$this->getServer()->loadLevel($level); //Register tiles and entities
+		$this->getServer()->loadLevel($level);
 		$world = $this->getServer()->getLevelByName($level);
-		$world->unload();
-          $this->getServer()->loadLevel($level);
+		$world->unload(); //If we don't unload and then load, entities and tiles may be make bug.
+          	$this->getServer()->loadLevel($level);
 	}
+	//TODO: better way for run jar files
 	private function anvilToRegion($level): void{
 		system("java -jar AnvilToRegion.jar worlds/".$level);
 	}
